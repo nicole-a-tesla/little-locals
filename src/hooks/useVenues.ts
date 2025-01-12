f667b286929102ce8c6420eec3e6fcb047d1cde2
nicole mccabe <thisisnotnicole@gmail.com>
2025-01-12 13:45:19 +0000

    use builtin contains method

--- src/hooks/useVenues.ts
+++ src/hooks/useVenues.ts
@@ -17,16 +17,7 @@ function filterVenues(venues, bounds) {
     return venues;
   }
 
-  const maxLat = Math.max(bounds._sw.lat, bounds._ne.lat);
-  const minLat = Math.min(bounds._sw.lat, bounds._ne.lat);
-
-  const maxLng = Math.max(bounds._sw.lng, bounds._ne.lng);
-  const minLng = Math.min(bounds._sw.lng, bounds._ne.lng);
-
   return venues.filter(v => {
-    return v.latitude <= maxLat
-      && v.latitude >= minLat
-      && v.longitude <= maxLng
-      && v.longitude >= minLng;
+    return bounds.contains([v.longitude, v.latitude])
   })
 }
